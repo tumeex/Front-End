7a892feb292c5b8bffc4938b6256fac8dc4bb72c
TUPEDD <opelmove@gmail.com>
2021-02-26 13:59:17 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBrand = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+const body = document.querySelector('main');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBrand.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBrand.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        showMenu = false;
+    }
+}
+
+body.addEventListener('click', closeMenu);
+
+function closeMenu() {
+    if (showMenu) {
+        toggleMenu();
+    }
+}
